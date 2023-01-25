54d50e4790a09f87f2ccdae600c64893fbec4eb1
vikimark <mark.phurit@gmail.com>
2023-01-25 23:06:24 +0700

    done: 04_Loop_Bisection_Log10

---
+++ 04_Loop/04_Loop_★_Bisection_Log10.py
@@ -0,0 +1,11 @@
+a=float(input())
+u=a
+l=0
+x=((u+l)/2.)
+while abs(a-10**x) > 10**-10*max(a, x**2):
+    if 10**x > a:
+        u = x
+    elif 10**x < a:
+        l = x
+    x = (u+l)/2.
+print(round(x, 6))
