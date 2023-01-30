2512a7fad52349a7b2ed185294230ff3ae2db96b
vikimark <mark.phurit@gmail.com>
2023-01-30 10:56:50 +0700

    fixed: suit all testcase

--- 04_Loop/04_Loop_★_Average.py
+++ 04_Loop/04_Loop_★_Average.py
@@ -1,9 +1,12 @@
 sum = 0.
 length = 0
 a = input()
-while a != 'q':
-    sum += float(a)
-    length += 1
-    a = input()
+if a == 'q':
+    print("No Data")
+else:
+    while a != 'q':
+        sum += float(a)
+        length += 1
+        a = input()
 
-print(round(sum/length, 2))
+    print(round(sum/length, 2))
