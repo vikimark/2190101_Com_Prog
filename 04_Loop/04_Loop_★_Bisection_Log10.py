106c68884285ee3439c267619671bc1a2029d2e7
vikimark <mark.phurit@gmail.com>
2023-01-31 09:17:59 +0700

    fixed: max function argument

--- 04_Loop/04_Loop_★_Bisection_Log10.py
+++ 04_Loop/04_Loop_★_Bisection_Log10.py
@@ -2,7 +2,7 @@ a=float(input())
 u=a
 l=0
 x=((u+l)/2.)
-while abs(a-10**x) > 10**-10*max(a, x**2):
+while abs(a-10**x) > 10**-10*max(a, 10**x):
     if 10**x > a:
         u = x
     elif 10**x < a:
