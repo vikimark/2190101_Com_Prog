993e1ccd95dc23a55b8c75811cc9d408c89e4eee
vikimark <mark.phurit@gmail.com>
2023-02-03 14:54:00 +0700

    done: 05_List_Peak

---
+++ 05_List/05_List_★_Peaks.py
@@ -0,0 +1,8 @@
+peaks = 0
+
+nums = [int(num) for num in input().split()]
+for i, num in enumerate(nums[1:-1]):
+    print(nums[i], num, nums[i+2])
+    if num > nums[i] and num > nums[i+2]:
+        peaks+=1
+print(peaks)
