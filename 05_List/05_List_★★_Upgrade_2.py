3df6981d0ef8be42e339aa7090092fda7e57bc70
vikimark <mark.phurit@gmail.com>
2023-02-05 22:02:11 +0700

    Done: 05_List_Upgrade_2

---
+++ 05_List/05_List_★★_Upgrade_2.py
@@ -0,0 +1,16 @@
+grades=['A','B+','B','C+','C','D+','D','F']
+ids=[]
+a = input()
+while a != 'q':
+    a = a.split()
+    ids.append([int(a[0]), grades.index(a[1])])
+    a = input()
+
+for id in input().split():
+    for info in ids:
+        if int(id) == info[0]:
+            info[1] = info[1]-1 if info[1] != 0 else info[1]
+ids = sorted(ids, key=lambda x: x[0])
+
+for info in ids:
+    print(info[0], grades[info[1]])
