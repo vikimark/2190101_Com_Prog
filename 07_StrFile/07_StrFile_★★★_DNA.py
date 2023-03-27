8e16f945aab45aead4482a57cb7ee8aa55f72bf5
vikimark <mark.phurit@gmail.com>
2023-03-27 12:32:13 +0700

    done DNA

--- 07_StrFile/07_StrFile_★★★_DNA.py
+++ 07_StrFile/07_StrFile_★★★_DNA.py
@@ -1,3 +1,30 @@
-a = input()
-b = input()
-print("YES" if sorted(a)==sorted(b) else "NO")
+def overlap_occurence(string, sub):
+    count = start = 0
+    while True:
+        start = string.find(sub, start) + 1
+        if start > 0:
+            count+=1
+        else:
+            return count
+
+dna = input().upper()
+command = input()
+if command == 'D':
+    pair = input()
+valid = True
+for ch in dna:
+    valid &= ch in ['A', 'T', 'C', 'G']
+if not valid:
+    print("Invalid DNA")
+elif command == 'R':
+    dna = dna.replace('A', 't').replace('T', 'a').replace('C', 'g').replace('G', 'c')
+    dna = dna.upper()[::-1]
+    print(dna)
+elif command == 'F':
+    occur = []
+    chs = ['A', 'T', 'G', 'C']
+    for ch in chs:
+        occur.append(dna.count(ch))
+    print("A={}, T={}, G={}, C={}".format(*occur))
+elif command == 'D':
+    print(overlap_occurence(dna, pair))
