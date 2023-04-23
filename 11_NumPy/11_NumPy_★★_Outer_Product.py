67174a8a2adf167e0524e054dab0105f82ad686e
vikimark <mark.phurit@gmail.com>
2023-04-23 19:19:40 +0700

    change element-wise mul ot matmul

--- 11_NumPy/11_NumPy_★★_Outer_Product.py
+++ 11_NumPy/11_NumPy_★★_Outer_Product.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def mult_table(nrows, ncols):
-    return (np.arange(nrows)+1)[..., np.newaxis] * (np.arange(ncols)+1)
+    return (np.arange(nrows)+1)[..., np.newaxis].dot((np.arange(ncols)+1)[...,np.newaxis].T)
 
 exec(input().strip())
 # print(mult_table(2,2))
